5aefaf5efea4fe36fed99532e17d11d1e928a22e
Yrtna <dummyauction42@gmail.com>
2020-09-29 22:44:05 +0200

    Added Runtime generation of Shader variants with MaterialPropertyBlocks ands GPU instancing

---
+++ Assets/AssetShaderSpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetShaderSpawnerScript : MonoBehaviour
+{
+    public int numberToSpawn = 1;
+    public float spacing = 1.5f;
+    public GameObject go;
+    public Vector3 direction;
+
+    private static readonly int EmissionColor = Shader.PropertyToID("EmissionColor");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var colorStep = 1f / numberToSpawn;
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            var obj = Instantiate(go, transform.position + (direction * spacing) * i, Quaternion.identity, this.transform);
+            var propertyBlock = new MaterialPropertyBlock();
+            var componentInChildren = obj.GetComponentInChildren<Renderer>();
+
+            propertyBlock.SetColor(EmissionColor, Random.ColorHSV(colorStep*i, colorStep*i, 0.8f, 1f, 0.8f, 1f)*2f);
+            componentInChildren.SetPropertyBlock(propertyBlock);
+        }
+    }
+}
---
+++ Assets/ColorPropertySetter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPropertySetter : MonoBehaviour
+{
+    [ColorUsage(true, true)]
+    public Color MaterialColor;
+    private MaterialPropertyBlock propertyBlock;
+
+    // OnValidate is called in the editor after the component is edited
+    void OnValidate()
+    {
+        if (propertyBlock == null)
+            propertyBlock = new MaterialPropertyBlock();
+
+        var renderer = GetComponentInChildren<Renderer>();
+        propertyBlock.SetColor("Color_4E2ABACF", MaterialColor);
+        renderer.SetPropertyBlock(propertyBlock);
+    }
+}
