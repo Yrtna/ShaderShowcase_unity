2f6dbac18229950d94be154c70aff76ea0d763cf
Yrtna <dummyauction42@gmail.com>
2020-09-30 00:18:51 +0200

    Added Dissolve Images

--- Assets/AssetShaderSpawnerScript.cs
+++ Assets/AssetShaderSpawnerScript.cs
@@ -6,6 +6,7 @@ public class AssetShaderSpawnerScript : MonoBehaviour
 {
     public int numberToSpawn = 1;
     public float spacing = 1.5f;
+    public int maxPerLine = 1;
     public GameObject go;
     public Vector3 direction;
 
@@ -15,14 +16,26 @@ public class AssetShaderSpawnerScript : MonoBehaviour
     void Start()
     {
         var colorStep = 1f / numberToSpawn;
+
+        var currentLine = 0;
+        var currentPos = 0;
         for (int i = 0; i < numberToSpawn; i++)
         {
-            var obj = Instantiate(go, transform.position + (direction * spacing) * i, Quaternion.identity, this.transform);
+            if (currentPos > maxPerLine)
+            {
+                currentLine += 1;
+                currentPos = 0;
+            }
+            var obj = Instantiate(go,
+                transform.position + ((Vector3.forward * spacing * currentPos) + (Vector3.right * spacing * currentLine)),
+                Quaternion.identity, this.transform);
             var propertyBlock = new MaterialPropertyBlock();
             var componentInChildren = obj.GetComponentInChildren<Renderer>();
 
-            propertyBlock.SetColor(EmissionColor, Random.ColorHSV(colorStep*i, colorStep*i, 0.8f, 1f, 0.8f, 1f)*2f);
+            propertyBlock.SetColor(EmissionColor,
+                Random.ColorHSV(colorStep * i, colorStep * i, 0.8f, 1f, 0.8f, 1f) * 2f);
             componentInChildren.SetPropertyBlock(propertyBlock);
+            currentPos += 1;
         }
     }
 }
